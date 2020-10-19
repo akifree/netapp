1a4d246a66676e14e56d44c544eb9525616a22bc
akifree <unite0728rainy@gmail.com>
2020-10-19 17:16:02 +0900

    feat: Add exception handling

--- win-speedtest.py
+++ win-speedtest.py
@@ -2,43 +2,41 @@
 
 def speedTest():
     import subprocess
-    import sys
     import re
     import json
 
     import win_json_to_csv
 
+    # speedtestコマンドの実行、結果を標準出力する。
+    try:
+        data = subprocess.check_output(['speedtest', '-f', 'json'], encoding='utf-8')
+    except Exception:
+        pass
+    else:
+        data = re.sub('\n', "", data)
+        # print(data)
 
-    cp = subprocess.run(['speedtest', '-f', 'json'], encoding='utf-8', stdout=subprocess.PIPE)
-    if cp.returncode != 0:
-        print('speed test faild', file=sys.stderr)
-        sys.exit(1)
+        data = json.loads(data)  # JSON文字列を辞書に変換
 
+        json_data = open('win-output.json', mode='w')
+        json.dump(data, json_data)  # 辞書をJSONファイルとして保存
+        json_data.close()
 
-    data = cp.stdout
-    data = re.sub('\n', "", data)
-    # print(data)
+        # Mbpsに変換
+        download = int(data["download"]["bandwidth"]) * 8 / 1000 /1000
+        upload = int(data["upload"]["bandwidth"]) * 8 / 1000 /1000
+        download = round(download, 2)
+        upload = round(upload, 2)
+        print("    Download: {}Mbps, Upload: {}Mbps".format(download, upload))
 
-    data = json.loads(data)
-
-    json_data = open('win-output.json', mode='w')
-    json.dump(data, json_data)
-    json_data.close()
-
-
-    download = int(data["download"]["bandwidth"]) * 8 / 1000 /1000
-    upload = int(data["upload"]["bandwidth"]) * 8 / 1000 /1000
-    download = round(download, 2)
-    upload = round(upload, 2)
-    print("Download: {}Mbps, Upload: {}Mbps".format(download, upload))
-
-
-    win_json_to_csv.change()
+        # CSVファイルへの出力
+        win_json_to_csv.change()
 
 
 if __name__ == '__main__':
     import time
+    import datetime
     for i in range(1, 168, 1):
-        time.sleep(3600)
-        print(i, "時間経過")
+        time.sleep(60)
+        print(datetime.datetime.now())
         speedTest()
